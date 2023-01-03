66ed776bc4c5d68d76ddfb0cd4c1dafe60203403
git-sergBond <git-serg.bond@yandex.ru>
2023-01-03 23:24:59 +0300

    GOERLI_TEST_NET

--- nodejs/Web3/index.js
+++ nodejs/Web3/index.js
@@ -1,11 +1,11 @@
 const INFRA_API_KEY = "d23be88a818f4fd9aa03691a48ccf714";
 const SEPOLIA_TEST_NET = "https://sepolia.infura.io/v3/";
-const HTTP_PROVIDER = SEPOLIA_TEST_NET + INFRA_API_KEY;
+const GOERLI_TEST_NET = "https://goerli.infura.io/v3/";
+const HTTP_PROVIDER = GOERLI_TEST_NET + INFRA_API_KEY;
 
 var Web3 = require('web3')
 
 var web3 = new Web3(new Web3.providers.HttpProvider(HTTP_PROVIDER));
-
 
 function createAccounts() {
     let AliceKeys = web3.eth.accounts.create();
@@ -16,8 +16,10 @@ function createAccounts() {
 
 //step 0.
 //uncomment line code below to create accounts Alice & Bob
-//createAccounts();
+createAccounts();
 
+//for https://sepolia.dev/
+/*
 var AliceKeys = {
     address: '0xde8075611a729Be46D1acB43C43bdA7Ea9Cb8483',
     privateKey: '0xcab834d350f6aed82a2a5f23af7af1fcbd73b0979521746f521e9028b6523bd0',
@@ -26,6 +28,18 @@ var AliceKeys = {
 var BobKeys = {
     address: '0x002A4f67d1965b5be5E3A5C83F92983BfeE2280d',
     privateKey: '0x001487eb4bb6b977dd8fffddbf4655082b15bf1dcdc2b7b0e4f8b4008a3dd04c',
+}
+*/
+
+//for https://goerlifaucet.com/
+var AliceKeys = {
+    address: '0x73C2Ca66fa68A8cf7baACb70462699438279F202',
+    privateKey: '0x6f5cb81a8a402d030c6f59365c4b482304bb7a2bf5b47731b117954fe157aa9f',
+}
+
+var BobKeys = {
+    address: '0x16a97719deD7aBBf63ec983FCBf35Adbd7EEbd63',
+    privateKey: '0xd23d7174674feb0494b9ca322049936c492f28aa8731d01d2c81579d45057c40',
 }
 
 async function getBalance(address) {
@@ -36,4 +50,4 @@ async function getBalance(address) {
 //step 1.
 //send some money to Alice with test network faucet
 //and check this by line code below
-getBalance(AliceKeys.address);
+//getBalance(AliceKeys.address);
