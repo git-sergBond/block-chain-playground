d5a826a3f191d687d2e478b8207c4992bca21bca
git-sergBond <git-serg.bond@yandex.ru>
2023-01-03 23:15:05 +0300

    save keys

--- nodejs/Web3/index.js
+++ nodejs/Web3/index.js
@@ -14,14 +14,17 @@ function createAccounts() {
     console.log(BobKeys);
 }
 
+//use this function once to create accounts Alice & Bob
 //createAccounts();
-/*
-OUTPUT:
-AliceKeys:
-address: '0xde8075611a729Be46D1acB43C43bdA7Ea9Cb8483',
-privateKey: '0xcab834d350f6aed82a2a5f23af7af1fcbd73b0979521746f521e9028b6523bd0',
 
-BobKeys:
-address: '0x002A4f67d1965b5be5E3A5C83F92983BfeE2280d',
-privateKey: '0x001487eb4bb6b977dd8fffddbf4655082b15bf1dcdc2b7b0e4f8b4008a3dd04c',
- */
+var AliceKeys = {
+    address: '0xde8075611a729Be46D1acB43C43bdA7Ea9Cb8483',
+    privateKey: '0xcab834d350f6aed82a2a5f23af7af1fcbd73b0979521746f521e9028b6523bd0',
+}
+
+var BobKeys = {
+    address: '0x002A4f67d1965b5be5E3A5C83F92983BfeE2280d',
+    privateKey: '0x001487eb4bb6b977dd8fffddbf4655082b15bf1dcdc2b7b0e4f8b4008a3dd04c',
+}
+
+
