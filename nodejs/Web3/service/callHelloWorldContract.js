1eb36f9ae8a2d1d29da3659b5941eadf52926569
git-sergBond <git-serg.bond@yandex.ru>
2023-01-04 23:11:01 +0300

    begin callHelloWorldContract

---
+++ nodejs/Web3/service/callHelloWorldContract.js
@@ -0,0 +1,8 @@
+var web3 = require("./web3Provider");
+var { abi, byteCode } = require("../contracts/HelloWorld");
+
+function callHelloWorldContract() {
+
+}
+
+module.exports = callHelloWorldContract;
