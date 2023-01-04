9624819a40ab34a752032c4a00a0a10f5475ccc7
git-sergBond <git-serg.bond@yandex.ru>
2023-01-04 23:18:13 +0300

    comments

--- nodejs/Web3/index.js
+++ nodejs/Web3/index.js
@@ -6,31 +6,19 @@ var deployHelloWorldContractWay1 = require("./service/deployHelloWorldContractWa
 var deployHelloWorldContractWay2 = require("./service/deployHelloWorldContractWay2");
 
 //==========CREATE ACCOUNTS==========
-//step 0.
-//uncomment line code below to create accounts Alice & Bob. Copy info from log to nodejs/Web3/constants/accounts.js
-//createAccounts();
+//step 0. uncomment this line: createAccounts();
+//step 1. copy info from log to nodejs/Web3/constants/accounts.js
 
 //==========GET BALANCE==========
-//step 1.
-//send some money to Alice with test network faucet
-//and check this by line code below
-//getBalance(AliceKeys.address);
-//getBalance(BobKeys.address);
-/*
-AliceKeys
-address: 0x73C2Ca66fa68A8cf7baACb70462699438279F202
-getBalance: 194800000000000000
-BobKeys
-address: 0x16a97719deD7aBBf63ec983FCBf35Adbd7EEbd63
-getBalance: 1000000000000000
-*/
+//step 2. uncomment this line: getBalance(AliceKeys.address);
+//step 3. uncomment this line:  getBalance(BobKeys.address);
 
 //==========SEND MONEY==========
-//step 2. test transaction from Alice to Bob
-//transactionFromAliceToBob();
+//step 4. uncomment this line: transactionFromAliceToBob();
 
 //==========CREATE SMART-CONTRACT==========
-//step 3. Compile HelloWorld.sol in RemixIDE copy this info to contracts\HelloWorld.js
-//step 4. uncomment deployHelloWorldContractWay1();
-//step 5. uncomment deployHelloWorldContractWay2();
+//step 5. Compile HelloWorld.sol in RemixIDE.
+//step 6. copy result BYTE-CODE and ABI to contracts\HelloWorld.js
+//step 7. uncomment this line: deployHelloWorldContractWay1();
+//step 8. uncomment this line: deployHelloWorldContractWay2();
 
--- nodejs/Web3/service/getBalance.js
+++ nodejs/Web3/service/getBalance.js
@@ -6,4 +6,13 @@ async function getBalance(address) {
     console.log("getBalance: " + res);
 }
 
+/*
+AliceKeys
+address: 0x73C2Ca66fa68A8cf7baACb70462699438279F202
+getBalance: 194800000000000000
+BobKeys
+address: 0x16a97719deD7aBBf63ec983FCBf35Adbd7EEbd63
+getBalance: 1000000000000000
+*/
+
 module.exports = getBalance;
