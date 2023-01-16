b723f260c9a4af2dedbd098a88494fa5a8783d6e
git-sergBond <git-serg.bond@yandex.ru>
2023-01-16 12:44:58 +0300

    POST generateWallet

--- java/Web3Samples/src/main/java/ss/bond/Web3Samples/resource/GeneralController.java
+++ java/Web3Samples/src/main/java/ss/bond/Web3Samples/resource/GeneralController.java
@@ -2,13 +2,19 @@ package ss.bond.Web3Samples.resource;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.crypto.CipherException;
 import org.web3j.protocol.core.methods.response.EthAccounts;
 import org.web3j.protocol.core.methods.response.EthBlockNumber;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 import ss.bond.Web3Samples.service.GeneralService;
 
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -44,5 +50,13 @@ public class GeneralController {
     @GetMapping("/get-balance/{address}")
     EthGetBalance getEthBalance(@PathVariable String address) throws ExecutionException, InterruptedException {
         return generalService.getEthBalance(address);
+    }
+
+    /**
+     * @return name of file: UTC--2023-01-16T09-44-14.773919300Z--44d5597848fc7562d9fd6c9a53626f4626f105d1.json
+     */
+    @PostMapping("/generate-wallet")
+    String generateWallet() throws InvalidAlgorithmParameterException, CipherException, IOException, NoSuchAlgorithmException, NoSuchProviderException {
+        return generalService.generateWallet();
     }
 }
--- java/Web3Samples/src/main/java/ss/bond/Web3Samples/service/GeneralService.java
+++ java/Web3Samples/src/main/java/ss/bond/Web3Samples/service/GeneralService.java
@@ -1,6 +1,8 @@
 package ss.bond.Web3Samples.service;
 
 import org.springframework.stereotype.Service;
+import org.web3j.crypto.CipherException;
+import org.web3j.crypto.WalletUtils;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameter;
 import org.web3j.protocol.core.methods.response.EthAccounts;
@@ -8,10 +10,18 @@ import org.web3j.protocol.core.methods.response.EthBlockNumber;
 import org.web3j.protocol.core.methods.response.EthGetBalance;
 import org.web3j.protocol.core.methods.response.EthGetTransactionCount;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
 import java.util.concurrent.ExecutionException;
 
 @Service
 public class GeneralService {
+
+    private static final String KEY_STORE_PASSWORD = "password123";
+    private static final String KEY_STORE_PATH = "D:\\dev\\block-chain-playground\\java\\Web3Samples\\target";
 
     private final Web3j web3j;
 
@@ -33,5 +43,9 @@ public class GeneralService {
 
     public EthGetBalance getEthBalance(String address) throws ExecutionException, InterruptedException {
         return web3j.ethGetBalance(address, DefaultBlockParameter.valueOf("latest")).sendAsync().get();
+    }
+
+    public String generateWallet() throws InvalidAlgorithmParameterException, CipherException, IOException, NoSuchAlgorithmException, NoSuchProviderException {
+        return WalletUtils.generateNewWalletFile(KEY_STORE_PASSWORD, new File(KEY_STORE_PATH), true);
     }
 }
